89cd7bdbaf04767d5489813a8342966ba378a73b
mikelReinanArcardo <mikeljuly28@gmail.com>
2025-06-05 20:07:29 +0800

    chore: move assembler .asm test file

---
+++ test_all_instr.asm
@@ -0,0 +1,98 @@
+rot-r
+rot-l
+rot-rc
+rot-lc
+from-mba
+to-mba
+from-mdc
+to-mdc
+addc-mba
+add-mba
+subc-mba
+sub-mba
+inc*-mba
+dec*-mba
+inc*-mdc
+dec*-mdc
+and-ba
+xor-ba
+or-ba
+and*-mba
+xor*-mba
+or*-mba
+clr-cf
+set-cf
+set-ei
+clr-ei
+ret
+retc
+from-pa
+inc
+to-ioa
+to-iob
+to-ioc
+bcd
+shutdown
+timer-start
+timer-end
+from-timerl
+from-timerh
+to-timerl
+to-timerh
+nop
+dec
+
+#Special R-types (from test file)
+inc*-reg ra
+dec*-reg ra
+to-reg ra
+from-reg ra
+inc*-reg rB
+dec*-reg rB
+to-reg rB
+from-reg rB
+inc*-reg Rc
+dec*-reg Rc
+to-reg Rc
+from-reg Rc
+inc*-reg RD
+dec*-reg RD
+to-reg RD
+from-reg RD
+inc*-reg rE
+dec*-reg rE
+to-reg rE
+from-reg rE
+
+# I-types (from test file)
+add 2047
+sub 2047
+and 2047
+xor 2047
+or 2047
+r4 2047
+timer 2047
+rarb 2047
+rcrd 2047
+acc 2047
+
+# B-types (from test file)
+b-bit 111111 2047
+bnz-a 2047
+bnz-b 2047
+beqz 2047
+bnez 2047
+beqz-cf 2047
+bnez-cf 2047
+b-timer 2047
+bnz-d 2047
+b 2047
+call 2047
+
+# .byte directives
+.byte 0x00
+.byte 0xff
+.byte 0xf2
+.byte 0xeA
+.byte 0xBC
+.byte 0xD1
