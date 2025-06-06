0e922d219ea78eae0a62f269520987ae8bcfbabe
mikelReinanArcardo <mikeljuly28@gmail.com>
2025-06-06 15:29:52 +0800

    feat: allow to read labels in assembler

--- assembler.py
+++ assembler.py
@@ -7,17 +7,76 @@ if __name__ == "__main__":
     format = sys.argv[2]
     output = []
 
-    # read instructions
+    # keep track of label names:
+    labels = dict()
+
+    branch_instr = ["b-bit", "bnz-a", "bnz-b", "beqz", "bnez",
+                    "beqz-cf", "bnez-cf", "b-timer", "bnz-d", "b", "call"]
+
+    pc = 0
+
+    # find all labels first in the instruction file and track valid instructions
     with open(input_filename, "r") as f:
-        for line in f:
+        for i, line in enumerate(f):
             instr = line.strip().split(" ")
 
             # if whiteline or comment
             if not instr[0] or instr[0][0] in ["#", ";"]:
                 continue
 
+            # set label PC
+            elif instr[0][-1] == ':':
+                label = instr[0][:-1].lower()
+                if label not in labels:
+                    # NOTE: if +2 it's possible na last line is a label so it might overflow
+                    labels[label] = bin(pc)[2:].zfill(8)
+                pc += 2
+
+            # convert labels to immediate for branch instructions
+            elif instr[0] in branch_instr and not instr[1].isdigit():
+                pc += 2
+
             # .byte directive
-            if instr[0] == ".byte":
+            elif instr[0] == ".byte":
+                pc += 1
+            else:
+                # convert to machine code
+                machine_code = convert_inst(instr)
+                if not machine_code:
+                    raise SyntaxError(f"Invalid Instruction: {line}")
+                pc += len(machine_code)
+
+    # read instructions
+    with open(input_filename, "r") as f:
+        for line in f:
+            instr = line.strip().split(" ")
+
+            machine_code = []
+
+            # if whiteline or comment
+            if not instr[0] or instr[0][0] in ["#", ";"]:
+                continue
+
+            # ADDITIONAL LABEL INSTRUCTION
+            # USE CASE: Similar to comments, this should NOT be in-line with another instruction
+            elif instr[0][-1] == ':':
+                # set to unused machine code
+                machine_code = ["01001000"]
+                label = instr[0][:-1].lower()
+                machine_code.append(labels[label])
+
+            # convert labels to immediate for branch instructions
+            elif instr[0] in branch_instr and not instr[1].isdigit():
+                print(instr)
+                label = instr[1].lower()
+                if label not in labels:
+                    raise SyntaxError(f"{label} is an unknown label")
+                instr[1] = str(int(labels[label], 2))
+                print(instr)
+                machine_code = convert_inst(instr)
+
+            # .byte directive
+            elif instr[0] == ".byte":
                 machine_code = [bin(int(instr[1], 16))[2:].zfill(8)]
             else:
                 # convert to machine code
